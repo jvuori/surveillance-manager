b04863ba416bc86ff0b06def1ac180107edaf5b3
Jaakko Vuori <jaakko.vuori@gmail.com>
2024-04-29 22:13:56 +0300

    Move object detection logic to another module

---
+++ survin/det.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+from ultralytics import YOLO
+
+
+model = YOLO("yolov8s.pt")
+
+def detect_objects(source: Path, save: bool):
+    results = model.track(
+        source=source,
+        save=save,
+        conf=0.5,
+        # iou=0.5,
+        stream=True,
+        verbose=False,
+    )
+
+    SKIPPED_CLASSIFICATIONS = ["bench", "chair"]
+
+    classifications = set()
+    for result in results:
+        for classification in result.boxes.cls.cpu().tolist():
+            classification_text = model.names[int(classification)]
+            if classification_text not in SKIPPED_CLASSIFICATIONS:
+                classifications.add(classification_text)
+
+    return classifications
