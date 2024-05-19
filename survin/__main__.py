77dab591c4c0d685fbae3be28447770448d1459c
Jaakko Vuori <jaakko.vuori@gmail.com>
2024-05-19 23:03:29 +0300

    Accept also files

--- survin/__main__.py
+++ survin/__main__.py
@@ -3,15 +3,29 @@ from pathlib import Path
 
 from survin import det
 
-parser = argparse.ArgumentParser()
-parser.add_argument("source", type=Path, help="Path to the source directory")
-parser.add_argument("--save", action="store_true")
-args = parser.parse_args()
-if not args.source.is_dir():
-    parser.error("source must be a directory")
 
-for file_path in args.source.glob("**/*"):
-    if file_path.is_file():
-        detected_objects:set[str] = det.detect_objects(file_path, args.save)
-        if detected_objects:
-            print(file_path, detected_objects)
+def _handle_file(file_path: Path, save: bool) -> None:
+    detected_objects: set[str] = det.detect_objects(file_path, save)
+    if detected_objects:
+        print(file_path, detected_objects)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "source",
+        type=Path,
+        help="Path to the source file or directory",
+    )
+    parser.add_argument("--save", action="store_true")
+    args = parser.parse_args()
+    if args.source.is_dir():
+        for file_path in args.source.glob("**/*"):
+            if file_path.is_file():
+                _handle_file(file_path, args.save)
+    else:
+        _handle_file(args.source, args.save)
+
+
+if __name__ == "__main__":
+    main()
