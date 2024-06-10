f94342ccc9040e6b885c5866923d2ffa8d77f3dc
Jaakko Vuori <jaakko.vuori@gmail.com>
2024-06-10 11:58:19 +0300

    Handle files with NEW status

--- survin/__main__.py
+++ survin/__main__.py
@@ -3,8 +3,10 @@ from pathlib import Path
 
 from survin import det, database
 
+
 def _save_snapshot_picture_from_video(video_path: Path, save_path: Path):
     import cv2
+
     cap = cv2.VideoCapture(str(video_path))
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     desired_frame = total_frames // 4  # 25% of the video
@@ -13,6 +15,7 @@ def _save_snapshot_picture_from_video(video_path: Path, save_path: Path):
     if ret:
         cv2.imwrite(str(save_path), frame)
     cap.release()
+
 
 def _handle_file(file_path: Path, save: bool) -> None:
     file_size = file_path.stat().st_size
@@ -23,17 +26,24 @@ def _handle_file(file_path: Path, save: bool) -> None:
 
     if database.get_status(file_path) is None:
         database.add_file(file_path)
+
+    if database.get_status(file_path) == database.Status.NEW:
+        print("Processing file:", file_path)
         detected_objects: set[str] = det.detect_objects(file_path, save)
+        print("Detected objects:", detected_objects)
         database.set_classifications(file_path, detected_objects)
         database.set_status(file_path, database.Status.COMPLETED)
 
-        snapshot_file_path = Path("snapshots").joinpath(file_path.with_suffix(".jpg").name)
-        snapshot_file_path.parent.mkdir(parents=True, exist_ok=True)
-        print(snapshot_file_path)
+    snapshot_file_path = Path("snapshots").joinpath(file_path.with_suffix(".jpg").name)
+    snapshot_file_path.parent.mkdir(parents=True, exist_ok=True)
+    if not snapshot_file_path.exists():
+        print("Creating snapshot:", snapshot_file_path)
         _save_snapshot_picture_from_video(file_path, snapshot_file_path)
 
     if database.get_status(file_path) == database.Status.DELETED:
+        print("Deleting file:", file_path)
         file_path.unlink()
+
 
 def main():
     parser = argparse.ArgumentParser()
